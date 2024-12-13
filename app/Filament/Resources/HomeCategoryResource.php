49756055c16d737f5e153a3010b912036040e35a
karimgouda <dev.karimgouda@gmail.com>
2024-12-13 19:17:10 +0200

    add

--- app/Filament/Resources/HomeCategoryResource.php
+++ app/Filament/Resources/HomeCategoryResource.php
@@ -79,10 +79,10 @@ class HomeCategoryResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-//                 Tables\Actions\DeleteAction::make(),
+                 Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
-                // Tables\Actions\DeleteBulkAction::make(),
+                 Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
 
