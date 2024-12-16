cbc5bf3483a51ce5466cc24816d740e6eb0a567c
karimgouda <dev.karimgouda@gmail.com>
2024-12-16 19:26:43 +0200

    add

--- app/Filament/Resources/BuildResource.php
+++ app/Filament/Resources/BuildResource.php
@@ -74,10 +74,10 @@ class BuildResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                // Tables\Actions\DeleteAction::make(),
+                 Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
-                // Tables\Actions\DeleteBulkAction::make(),
+                 Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
 
