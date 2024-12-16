bfbbc0a44b9c055f84bbf29d15c02ea312245f5d
karimgouda <dev.karimgouda@gmail.com>
2024-12-16 19:48:08 +0200

    add

--- app/Filament/Resources/ContactResource.php
+++ app/Filament/Resources/ContactResource.php
@@ -70,10 +70,10 @@ class ContactResource extends Resource
             ])
             ->actions([
                 // Tables\Actions\EditAction::make(),
-                // Tables\Actions\DeleteAction::make(),
+                 Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
-                // Tables\Actions\DeleteBulkAction::make(),
+                 Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
 
