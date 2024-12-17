eff447f081c892f0f625000dbc8dfada91d1fb78
Ali Talaat <alitalaatali30@gmail.com>
2024-12-18 00:43:15 +0200

    fix

--- resources/views/web/frontend/pages/offer-details.blade.php
+++ resources/views/web/frontend/pages/offer-details.blade.php
@@ -5,18 +5,20 @@
 @section('content')
 <section>
     <div class="page-body-wrapper">
-        <div class="container my-5">
+        <div class=" my-5">
             <div style="max-width: 700px; top: -80px;" class="mx-auto text-secondary">
-                <h1 class="font-weight-bold text-dark text-center m-3" style="font-style: italic">{{$course->title}}</h1>
+                <h1 class="font-weight-bold text-dark text-center m-3" style="font-style: italic">{{$course->title}}
+                </h1>
                 <div class="my-3 d-flex align-items-center justify-content-between">
                 </div>
             </div>
-            <img class="w-75 my-3 text-center"
-                src="{{ asset('storage/'.$course->icon) }}" />
+            <div class="text-center">
+                <img class="my-3 text-center" src="{{ asset('storage/'.$course->icon) }}" />
+            </div>
 
-            <div style="max-width: 700px; top: -80px;" class="mx-auto text-secondary">
+            <div style="max-width: 700px; top: -80px;" class="mx-auto text-secondary ">
                 <p class="my-2" style="line-height: 2;">
-                   {{$course->description}}
+                    {{$course->description}}
                 </p>
 
             </div>
