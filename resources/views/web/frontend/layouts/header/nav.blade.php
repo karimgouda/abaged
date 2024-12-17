2e2f6c0f2b305a29b6b3a44e2db3d8629ea8b765
karimgouda <dev.karimgouda@gmail.com>
2024-12-18 00:42:37 +0200

    add

--- resources/views/web/frontend/layouts/header/nav.blade.php
+++ resources/views/web/frontend/layouts/header/nav.blade.php
@@ -1,9 +1,10 @@
+@php use Illuminate\Support\Facades\Route; @endphp
 <nav class="navbar navbar-expand-lg fixed-top" style="padding-block: 0rem;">
     <div class="container">
         <a class="navbar-brand" href="#"><img src="{{public_storage(settings('site_logo'))}}" width="50" height="150"
-                style="width: 150px;"></a>
+                                              style="width: 150px;"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"><i class="mdi mdi-menu"> </i></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
@@ -13,29 +14,30 @@
             </div>
             <ul class="navbar-nav ml-auto align-items-center">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#home">{{__('site.shared.Home')}} <span
+                    <a class="nav-link" href=" {{Route::currentRouteName() == 'site.offer-details' ? \route('site.home') : '#home'}}">{{__('site.shared.Home')}} <span
                             class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#services">{{__('site.shared.Services')}}</a>
+                    <a class="nav-link" href="  {{Route::currentRouteName() == 'site.offer-details' ? \route('site.home') : '#services'}}">{{__('site.shared.Services')}}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#about">{{__('site.shared.About Us')}}</a>
+                    <a class="nav-link" href="{{Route::currentRouteName() == 'site.offer-details' ? \route('site.home') : '#about'}}">{{__('site.shared.About Us')}}</a>
                 </li>
                 {{-- <li class="nav-item">--}}
                 {{-- <a class="nav-link" href="#projects">{{__('site.shared.Projects')}}</a>--}}
                 {{-- </li>--}}
                 <li class="nav-item">
-                    <a class="nav-link" href="#testimonial">{{__('site.shared.Testimonials')}}</a>
+                    <a class="nav-link" href="{{Route::currentRouteName() == 'site.offer-details' ? \route('site.home') : '#testimonial'}}">{{__('site.shared.Testimonials')}}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#plans">{{__('site.shared.Plans')}}</a>
+                    <a class="nav-link" href="{{Route::currentRouteName() == 'site.offer-details' ? \route('site.home') : '#plans'}}">{{__('site.shared.Plans')}}</a>
                 </li>
                 <li class="nav-item rounded-5 mx-4" style="width: 40px; height: 40px;">
                     <div class="lang">
                         <a
                             href="{{ LaravelLocalization::getLocalizedURL(app()->getLocale() === 'en' ? 'ar' : 'en', null, [], true) }}">
-                            <img src="{{ app()->getLocale() === 'en' ? asset('frontend/images/us.svg') : asset('frontend/images/eg.svg') }}"
+                            <img
+                                src="{{ app()->getLocale() === 'en' ? asset('frontend/images/us.svg') : asset('frontend/images/eg.svg') }}"
                                 alt="lang">
                         </a>
                     </div>
